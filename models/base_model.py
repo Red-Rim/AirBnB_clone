7daa06750478baa488f76b7b7879601429bf05c7
Red-Rim <salmaouakhir20@gmail.com>
2023-10-22 14:55:03 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,60 +1,53 @@
 #!/usr/bin/python3
 
+"""Defines BaseModel class"""
 
-"""Define FileStorage Class"""
-"""from models.amenity import Amenity"""
-from models.base_model import BaseModel
-from models.city import City
-import json
-import os
-"""from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User"""
-import sys
+from uuid import uuid4
+import models
+from datetime import datetime
 
-# Get the absolute path to your project's root directory
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 sys.path.append(project_root)
 print(sys.path)
 
-class FileStorage:
+class BaseModel:
     """
-    FileStorage class to data
+        BaseModel class define common attributes/methods for other classes
 
-    Attributes:
-        __file_path (str): the file to save obj to
-        __objects (dict): A dictionary to store instances
+        Args:
+            *args: Not used.
+            **kwargs: A dict of attributes.
     """
 
-    __file_path = "file.json"
-    __objects = {}
-
-    def all(self):
-        """returns dictionary of all stored objects"""
-
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """stores object on the objects dictionary"""
-        objt = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(objt, obj.id)] = obj
+    def __init__(self, *args, **kwargs):
+        """Initialize new BaseModel"""
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
-        """saves the objects dict on a json file"""
-        odct = FileStorage.__objects
-        obj_dict = {obj: odct[obj].to_dict() for obj in odct.keys()}
-        with open(FileStorage.__file_path, "w") as fil:
-            json.dump(obj_dict, fil)
+        """updates the pub instance updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
-    def reload(self):
-        """deserialize json to __objects, if it exists"""
-        try:
-            with open(FileStorage.__file_path) as fil:
-                obj_dict = json.load(fil)
-                for s in obj_dict.values():
-                    cls_name = s["__class__"]
-                    del s["__class__"]
-                    self.new(eval(cls_name)(**s))
-        except FileNotFoundError:
-            return
+    def __str__(self):
+        """string representation of a BaseModel instance"""
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def to_dict(self):
+        """returns a dict containing all keys/values of __dict__ of instance"""
+        _dict = {
+            key: val if key not in ["created_at", "updated_at"] else
+            val.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in self.__dict__.items()
+        }
+        _dict['__class__'] = self.__class__.__name__
+        return _dict
